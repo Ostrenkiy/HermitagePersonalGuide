aaeca28681a5e6ae5e4815ce91a879374d7a2743
yuliaazak <yuliaazak@gmail.com>
2017-10-21 16:03:14 +0300

    Added simple VK auth

--- HermitagePersonalGuide/AppDelegate.swift
+++ HermitagePersonalGuide/AppDelegate.swift
@@ -15,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        let vkStoryBoard = UIStoryboard(name: "VkLogin", bundle: nil)
+        let vkController = vkStoryBoard.instantiateInitialViewController()
+        
+        if let window = self.window {
+            window.rootViewController = vkController
+            window.makeKeyAndVisible()
+        }
+        
         // Override point for customization after application launch.
         return true
     }
@@ -41,6 +50,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        VKSdk.processOpen(url, fromApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String)
+
+        return true
+    }
 
 }
 
