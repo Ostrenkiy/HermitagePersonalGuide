f9a0a5b7b6a9958e43b12347f0a5441c22fbe251
yuliaazak <yuliaazak@gmail.com>
2017-10-21 19:19:48 +0300

    Exhibit VC

---
+++ HermitagePersonalGuide/ExhibitInfo/PictureTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PictureTableViewCell.swift
+//  HermitagePersonalGuide
+//
+//  Created by Андрей on 21.10.2017.
+//  Copyright © 2017 Ostrenkiy. All rights reserved.
+//
+
+import UIKit
+
+class PictureTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
