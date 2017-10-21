95518c39ca819361e26326a4e648c7516265f566
yuliaazak <yuliaazak@gmail.com>
2017-10-21 18:34:06 +0300

    Some for iBeacon

---
+++ HermitagePersonalGuide/iBeacon/iBeaconSerivce.swift
@@ -0,0 +1,13 @@
+//
+//  iBeaconSerivce.swift
+//  HermitagePersonalGuide
+//
+//  Created by Андрей on 21.10.2017.
+//  Copyright © 2017 Ostrenkiy. All rights reserved.
+//
+
+import UIKit
+
+class iBeaconSerivce: NSObject {
+
+}
